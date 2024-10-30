18c3dde721f7d953e82c8c01f13b23dfcbad9b9d
DuarteSilva22 <duartesilva22@outlook.com>
2024-10-30 12:03:31 +0000

    Update bootstrap.php

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -8,7 +8,10 @@ use Framework\App;
 
 use function App\Config\{registerRoutes, registerMiddleware};
 use App\Config\Paths;
+use Dotenv\Dotenv;
 
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
 $app = new App(Paths::SOURCE . "App/container-definitions.php");
 
